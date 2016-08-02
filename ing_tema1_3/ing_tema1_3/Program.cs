d02cfe7d0964af8012700be0c5b71c900034c1f3
Petru Ritivoiu <p.ritivoiu@gmail.com>
2016-08-02 15:32:22 +0300

    Add files via upload
    ing_tema_3

---
+++ ing_tema1_3/ing_tema1_3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ing_tema1_3
+{
+    class Program
+    {
+        public static string introduceYourself(string yourName)
+        {
+            return "Hi, my name is " + yourName + " and i'm speaking from a stored function";
+        }
+
+        //Delegate pentru functie stocata
+        delegate string introduceDelegate_1(string yourName);
+
+        //Delegat pentru fucntie inline
+        delegate string introduceDelegate_2(string yourName);
+
+        static void Main(string[] args)
+        {
+            introduceDelegate_1 delegate1 = introduceYourself;
+            introduceDelegate_2 delegate2 = delegate (string yourName)
+            {
+                return "Hi, my name is " + yourName + " and i'm speaking from an inline function";
+            };
+
+            //Apel functie stocata
+            Console.WriteLine(introduceYourself("Petru"));
+
+            //Apel delegat catre functie stocata
+            Console.WriteLine(delegate1.Invoke("Mihai"));
+
+            //Apel delegat catre functie inline
+            Console.WriteLine(delegate2.Invoke("Cristina"));
+
+        }
+    }
+}
